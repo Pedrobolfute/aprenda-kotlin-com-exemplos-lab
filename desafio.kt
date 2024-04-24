2af62aa82517dc4cb857f230ae025a8a77a1ba29
Pedro Henrique <35509700+Pedrobolfute@users.noreply.github.com>
2024-04-24 17:32:25 -0300

    implementacao do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,23 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Usuario(val nome: String)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} matriculado em $nome com SUCESSO!")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val Components = ConteudoEducacional("Components", 120, Nivel.INTERMEDIARIO)
+    val DOM = ConteudoEducacional("DOM", 120, Nivel.DIFICIL)
+    val algoritimo = ConteudoEducacional("Algoritmo", 60, Nivel.BASICO)
+    
+    val javaScriptDeveloper = Formacao("JavaScript Developer", listOf(algoritimo, DOM, Components))
+    
+    javaScriptDeveloper.matricular(Usuario("Pedro"))
+    
+    println(javaScriptDeveloper)
 }
